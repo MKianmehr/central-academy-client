3dedeabc3cd9faef236aa8fc503bbb6a9ee03d48
Mohammadreza Kianmehr <mohammad.r.kianmehr@gmail.com>
2022-12-29 09:16:15 +0330

    Feat: add cashProvider for mui

--- src/models/Props.ts
+++ src/models/Props.ts
@@ -1,4 +1,6 @@
 import React from "react"
+import type { AppProps } from 'next/app'
+import { EmotionCache } from '@emotion/react';
 
 interface children {
     children: React.ReactElement
@@ -10,4 +12,8 @@ export interface OnLineProp extends children {
 
 export interface GlobalProp extends children {
 
+}
+
+export interface MyAppProps extends AppProps {
+    emotionCache?: EmotionCache;
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 import GlobalProvider from '../Providers/GlobalProvider'
+import createEmotionCache from '../utils/createEmotionCache'
+import { CacheProvider } from '@emotion/react';
+import { MyAppProps } from '../models/Props'
 
-export default function App({ Component, pageProps }: AppProps) {
+const clientSideEmotionCache = createEmotionCache();
+
+export default function App(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
     <GlobalProvider>
-      <Component {...pageProps} />
+      <CacheProvider value={emotionCache}>
+        <Component {...pageProps} />
+      </CacheProvider>
     </GlobalProvider>
   )
 }
