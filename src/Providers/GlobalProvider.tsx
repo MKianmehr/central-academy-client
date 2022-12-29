594be9305bc7a35517bc4735db8b0e7987ee0f31
Mohammadreza Kianmehr <mohammad.r.kianmehr@gmail.com>
2022-12-29 09:40:31 +0330

    Feat: add usevh

--- src/Providers/GlobalProvider.tsx
+++ src/Providers/GlobalProvider.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import OnlineProvider from './OnlineProvider'
 import { GlobalProp } from '../models/Props'
 import useRtl from '../hooks/useRtl'
+import useVh from '../hooks/useVh'
 
 const GlobalProvider: React.FC<GlobalProp> = ({ children }) => {
     useRtl()
+    useVh()
     return (
         <OnlineProvider>
             {children}
---
+++ src/hooks/useVh.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react'
+
+const useVh = () => {
+    const [vh, setVh] = useState<number>()
+    useEffect(() => {
+        setVh(window.innerHeight * 0.01)
+        // Then we set the value in the --vh custom property to the root of the document
+    }, [(typeof window) === "object" && window.innerHeight])
+
+    useEffect(() => {
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }, [vh])
+
+    return vh
+}
+
+export default useVh
