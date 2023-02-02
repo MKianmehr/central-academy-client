ac7e7fc5ccdcc7074ab1c3d2e5b0eed4b7e2e61a
Mohammadreza Kianmehr <mohammad.r.kianmehr@gmail.com>
2023-02-02 06:57:40 +0330

    Fix: prevent direct access to create course steps

--- src/pages/course/create/[step].tsx
+++ src/pages/course/create/[step].tsx
@@ -1,30 +1,17 @@
 import React from 'react'
-import { GetStaticProps, GetStaticPaths } from 'next'
+import { NextPage } from 'next'
 import CourseCreateSteps from '../../../components/pages/CourseCreateSteps'
 
-const Steps = () => {
+const Steps: NextPage = () => {
     return (
         <CourseCreateSteps />
     )
 }
 
-export const getStaticProps: GetStaticProps = async ({ locale }) => {
-    return {
-        props: {
-
-        }
-    }
-}
-export const getStaticPaths: GetStaticPaths = async () => {
-    const steps = 4
-    const paths = []
-    for (let i = 1; i <= steps; i++) {
-        paths.push({ params: { step: `${i}` }, locale: 'en' }, { params: { step: `${i}` }, locale: 'fa' },)
-    }
-    return {
-        paths: paths,
-
-        fallback: false, // can also be true or 'blocking'
+Steps.getInitialProps = async ({ req, res }) => {
+    if (req) {
+        res?.writeHead(302, { Location: '/instructor/courses' })
+        res?.end()
     }
 }
 
