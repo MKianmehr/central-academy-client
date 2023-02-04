8c51038fff601f3c8eed02dd000a652037dad3b7
Mohammadreza Kianmehr <mohammad.r.kianmehr@gmail.com>
2023-02-04 16:03:54 +0330

    Fix: replace useLayoutEffect with useEffect in useVh

--- src/hooks/useVh.tsx
+++ src/hooks/useVh.tsx
@@ -1,22 +1,16 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useInsertionEffect, useLayoutEffect, useState } from 'react'
+
 
 const useVh = () => {
-    const [vh, setVh] = useState<number>()
-    useEffect(() => {
-        setVh(window.innerHeight * 0.01)
+    useLayoutEffect(() => {
+        document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
         const handleResize = () => {
-            setVh(window.innerHeight * 0.01)
+            document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
         }
         window.addEventListener("resize", handleResize)
         return () => window.removeEventListener('resize', handleResize);
         // Then we set the value in the --vh custom property to the root of the document
-    }, [(typeof window) === "object" && window.innerHeight])
-
-    useEffect(() => {
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-    }, [vh])
-
-    return vh
+    }, [(typeof window) === "object" && window.innerHeight]);
 }
 
 export default useVh
