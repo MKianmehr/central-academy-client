7e7f36921c750491a99533dd8e0f1b24d061d0fb
Mohammadreza Kianmehr <mohammad.r.kianmehr@gmail.com>
2023-03-02 11:11:06 +0330

    Feat: home page banner

--- src/components/pages/Teaching/index.tsx
+++ src/components/pages/Teaching/index.tsx
@@ -1,16 +1,39 @@
 import React from 'react'
 import styles from './styles.module.scss'
-import TeachingBanner from '../../commons/TeachingBanner'
 import TeachingReasons from './TeachingReasons'
 import Numbers from './Numbers'
+import Banner from '../../commons/Banner'
+import { useRouter } from 'next/router'
+import { useAppSelector } from '../../../redux/hooks'
+import useTranslation from "next-translate/useTranslation";
 
 const Teaching = () => {
+    const { t } = useTranslation('common');
+    const router = useRouter()
+    const user = useAppSelector(state => state.user)
+    const becomeInstructor = async () => {
+
+        if (Object.keys(user).length == 0) {
+            router.push('/login', {
+                query: { redirect: 'instructor/courses' }
+            })
+        } else {
+            router.push('/teaching/onBoarding')
+        }
+    }
 
 
 
     return (
         <div className={styles.container}>
-            <TeachingBanner />
+            <Banner
+                onClickButton={becomeInstructor}
+                title={t("Come teach with us")}
+                description={t("become an instructor and change lives including your own")}
+                buttonText={t("Get started")}
+                imageAlt={t("teaching banner")}
+                imageSrc='https://s.udemycdn.com/teaching/billboard-desktop-v4.jpg'
+            />
             <TeachingReasons />
             <Numbers />
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import NavBar from "../components/commons/Navbar"
 import SwipeableTempDrawer from "../components/commons/SwipeableTempDrawer"
+import HomePage from "../components/pages/HomePage"
 
 const Home = () => {
   return (
     <>
       <SwipeableTempDrawer />
       <NavBar />
+      <HomePage />
     </>
   )
 }
